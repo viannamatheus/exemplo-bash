434e326d1d5e15e16abca6c13b54580578b41810
viannamatheus <viannamatheus351@gmail.com>
2024-05-18 01:43:34 +0000

    arquivo de exemplo

---
+++ exemplo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+echo"tempo de uso do PC"
+# chamada de comandos 
+# uptime
+# var=teste
+# echo "$var"
+echo"digite um numero inteiro"
+readnumero 
+ for num in {1..10}
+ do 
+ echo $numero " * " $num " = " $(($numero * $num))
+echo" numero digitado : $numero"
